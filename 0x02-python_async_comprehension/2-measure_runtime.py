d336370a3f3c898e8ab0a2dc3d6f9892fccf9fa9
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-10-15 23:49:22 +0100

    async comprehension tasks

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Measure times """
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+        measure time and execute in paralallel
+
+        Args:
+            void
+
+        Return:
+            Time elapsed in generating 4 floats asynchronously
+    """
+    first_time = time.perf_counter()
+    tasks = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*tasks)
+    elapsed = time.perf_counter()
+
+    return (elapsed - first_time)
---
+++ 0x02-python_async_comprehension/README.md
@@ -0,0 +1,15 @@
+# 0x02. Python - Async Comprehension
+
+## Description
+- How to write an asynchronous generator
+- How to use async comprehensions
+- How to type-annotate generators
+
+## Tasks
+* [0-async_generator.py](0-async_generator.py)
+* [0-main.py](0-main.py)
+* [1-async_comprehension.py](1-async_comprehension.py)
+* [1-main.py](1-main.py)
+* [2-main.py](2-main.py)
+* [2-measure_runtime.py](2-measure_runtime.py)
+* [README.md](README.md)
